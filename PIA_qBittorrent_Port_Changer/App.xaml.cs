c33d09480f2f16b67d11d356a21406c8dda34c42
Patrick Cartwright <pcartwright1981@gmail.com>
2024-08-25 19:01:15 -0500

    Change tray icon to resource.

--- PIA_qBittorrent_Port_Changer/App.xaml.cs
+++ PIA_qBittorrent_Port_Changer/App.xaml.cs
@@ -2,11 +2,8 @@
 {
     using H.NotifyIcon;
     using Microsoft.Win32;
-    using System.IO;
     using System.Timers;
     using System.Windows;
-    using System.Windows.Controls;
-    using System.Windows.Data;
 
     /// <summary>
     /// Interaction logic for App.xaml
@@ -19,18 +16,10 @@
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            var icon = PIA_qBittorrent_Port_Changer.Properties.Resources.Icon;
-            ShutdownMode = ShutdownMode.OnExplicitShutdown;
-
-            // Create tray.
-            notifyIcon = new TaskbarIcon
-            {
-                Icon = new System.Drawing.Icon(new MemoryStream(icon)),
-                DataContext = trayIconViewModel,
-                ContextMenu = BuildMenu()
-            };
+            notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+            notifyIcon.DataContext = trayIconViewModel;
             notifyIcon.ForceCreate();
-            
+
             // Set up timer for check.
             PortChecker portChecker = new(trayIconViewModel);
             var aTimer = new Timer();
@@ -50,41 +39,7 @@
                 };
             };
         }
-
-        private static ContextMenu BuildMenu()
-        {
-
-            var menu = new ContextMenu();
-            var menuItem = new MenuItem();
-            Binding myBinding = new Binding("PiaPortDisplay")
-            {
-                Source = trayIconViewModel
-            };
-            menuItem.SetBinding(HeaderedItemsControl.HeaderProperty, myBinding);   
-            menu.Items.Add(menuItem);
-
-            menuItem = new MenuItem();
-            myBinding = new Binding("QbitPortDisplay")
-            {
-                Source = trayIconViewModel
-            };
-            menuItem.SetBinding(HeaderedItemsControl.HeaderProperty, myBinding);
-            menu.Items.Add(menuItem);
-
-            menu.Items.Add(new MenuItem
-            {
-                Header = "Check Now",
-                Command = trayIconViewModel.CheckNowCommand
-            });
-            menu.Items.Add(new Separator());
-            menu.Items.Add(new MenuItem
-            {
-                Header = "Exit",
-                Command = trayIconViewModel.ExitCommand
-            });
-            return menu;
-        }
-
+      
         protected override void OnExit(ExitEventArgs e)
         {
             notifyIcon?.Dispose(); //the icon would clean up automatically, but this is cleaner
